34581b178f5d56c8b291c148d06b1c90140f53f1
Jackmc <jackmc1122@gmail.com>
2019-05-06 14:16:10 -0700

    we changed some styling in our index.html and we called the sign-up api

--- src/home/index.js
+++ src/home/index.js
@@ -1,4 +1,5 @@
 import makeUser from './make-user.js';
+import api from '../services/api.js';
 
 const userSignUp = document.getElementById('form');
 
@@ -6,4 +7,5 @@ userSignUp.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(userSignUp);
     const user = makeUser(formData);
+    api.signUp(user);
 });
