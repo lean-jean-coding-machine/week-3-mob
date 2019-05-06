80a1113231af477d24767570f4af83fbc960d47c
Jackmc <jackmc1122@gmail.com>
2019-05-06 13:07:00 -0700

    we successfully TDD-ed our makeUser function

--- test/make-user.test.js
+++ test/make-user.test.js
@@ -2,6 +2,15 @@ const test = QUnit.test;
 
 QUnit.module('makeUser');
 
+function makeUser(formData) {
+    const user = {
+        name: formData.get('name'),
+        buddy: formData.get('buddy'),
+        mood: 30,
+        treats: 1
+    };
+    return user;
+}
 test('makeUser function creates user object from form data', (assert) => {
     //Arrange
     const formData = new FormData();
