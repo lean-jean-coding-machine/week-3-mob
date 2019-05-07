45a89432ba0803ee4506633a848a4865c976bb17
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-07 15:16:57 -0700

    Move loadProfile into its own function.

--- src/map/map.js
+++ src/map/map.js
@@ -1,17 +1,4 @@
 import api from '../services/api.js';
+import loadProfile from '../load-profile.js';
 
-const username = document.getElementById('username');
-const buddy = document.getElementById('animal-avatar');
-const mood = document.getElementById('mood');
-const treats = document.getElementById('treats');
-
-const user = api.getUser();
-
-if(!user) {
-    window.location = '../../';
-}
-
-username.textContent = user.name;
-buddy.src = '../../assets/' + user.buddy + '.png';
-mood.textContent = user.mood;
-treats.textContent = user.treats;
+loadProfile();
