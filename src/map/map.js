263b242b991a11e879914775b26b3aaff6304dd4
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-07 15:24:08 -0700

    loaded quest list dynamically

--- src/map/map.js
+++ src/map/map.js
@@ -1,4 +1,15 @@
 import api from '../services/api.js';
 import loadProfile from '../load-profile.js';
+import createQuestLink from './create-quest-link.js';
 
 loadProfile();
+
+const nav = document.getElementById('quests');
+
+const quests = api.getQuests();
+//loop over quest array
+for(let i = 0; i < quests.length; i++) {
+    //create links to append to the nav (parent element)
+    const questLink = createQuestLink(quests[i]);
+    nav.appendChild(questLink);
+}
