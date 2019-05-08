d5c4ed82e1b579baa72ee00a2ccb2128ff872e2a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-07 17:00:41 -0700

    load quest from quest data.

---
+++ src/quest/quest.js
@@ -0,0 +1,25 @@
+import api from '../services/api.js';
+import createChoice from './create-choice.js';
+import loadProfile from '../load-profile.js';
+
+loadProfile();
+
+const choiceParent = document.getElementById('choices');
+const image = document.getElementById('quest-img');
+const title = document.getElementById('quest-title');
+const description = document.getElementById('description');
+
+const searchParams = new URLSearchParams(window.location.search);
+const questId = searchParams.get('id');
+
+const quest = api.getQuest(questId);
+console.log(description);
+title.textContent = quest.title;
+image.src = '../../assets/quest-photos/' + quest.image;
+description.textContent = quest.description;
+
+for(let i = 0; i < quest.choices.length; i++) {
+    const choice = createChoice(quest.choices[i]);
+    choiceParent.appendChild(choice);
+}
+
