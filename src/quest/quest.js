686ce2f18177a6b4663eaf5e3e32bfd868c748a4
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-07 17:47:55 -0700

    begin updating user profile after making a choice.

--- src/quest/quest.js
+++ src/quest/quest.js
@@ -1,7 +1,7 @@
 import api from '../services/api.js';
 import createChoice from './create-choice.js';
 import loadProfile from '../load-profile.js';
-// import scoreQuest from './score-quest.js';
+import scoreQuest from './score-quest.js';
 
 loadProfile();
 
@@ -9,6 +9,8 @@ const choiceParent = document.getElementById('choices');
 const image = document.getElementById('quest-img');
 const title = document.getElementById('quest-title');
 const description = document.getElementById('description');
+const formChoice = document.getElementById('quest-form');
+const user = api.getUser();
 
 const searchParams = new URLSearchParams(window.location.search);
 const questId = searchParams.get('id');
@@ -23,3 +25,17 @@ for(let i = 0; i < quest.choices.length; i++) {
     choiceParent.appendChild(choice);
 }
 
+formChoice.addEventListener('submit', event => {
+    event.preventDefault();
+    // const formData = new FormData(formChoice);
+    // const choiceId = formData.get('quest-option');
+    // let choice = {};
+    // for(let i = 0; i < quest.choices.length; i++) {
+    //     if(quest.choices[i].id === choiceId) {
+    //         choice = quest.choices[i];
+    //     }
+    // }
+    // scoreQuest(user, choice);
+    // api.saveUser();
+    // loadProfile();
+});
