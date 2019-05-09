c1540e213cc40969759acd7cccb7a71af25f3343
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-05-09 16:07:32 -0700

    we made our dictionaries for mood and treats, and utilized them in our end.js file

---
+++ src/end/mood-dictionary.js
@@ -0,0 +1,7 @@
+const moodDictionary = {
+    sad: 'Take a depression nap. Try again later...',
+    average: 'You are doing okay, but there are still more animals to be pet-ed. Make haste!',
+    happy: 'You did gooooood! You increased both your mood and the animals`. But why stop there?'
+};
+
+export default moodDictionary;
