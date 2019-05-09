59264cef2df32b350760d1457c10c05f339cbae6
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-05-09 14:32:35 -0700

    Imported our isSad and hasCompletedAllQuests functions and use them to test for game end."

--- src/map/map.js
+++ src/map/map.js
@@ -2,6 +2,8 @@ import api from '../services/api.js';
 import loadProfile from '../load-profile.js';
 import createQuestLink from './create-quest-link.js';
 import createCompletedQuest from './create-completed-quest.js';
+import isSad from './is-sad.js';
+import hasCompletedAllQuests from './has-completed-all-quests.js';
 
 loadProfile();
 
@@ -9,6 +11,10 @@ const nav = document.getElementById('quests');
 
 const quests = api.getQuests();
 const user = api.getUser();
+
+if(isSad(user) || hasCompletedAllQuests(user, quests)) {
+    window.location = '../end/end.html';
+}
 //loop over quest array
 for(let i = 0; i < quests.length; i++) {
     //create links to append to the nav (parent element)
